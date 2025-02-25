6afc76ffe66a3a23524b585f0425c8c46a95a10d
bluntchann <sibyamal17@gmail.com>
2025-02-25 13:09:45 +0530

    Add project files.

---
+++ new test/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace new_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
